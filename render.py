a7f38b1ac70306d254937aa2761ca418c7f0a704
Jan Czarnowski <jancz@tuta.io>
2019-06-07 14:09:53 +0100

    Get it working WIP

--- render.py
+++ render.py
@@ -2,11 +2,15 @@ import sys
 sys.path.insert(0,'./PyGLer/src')
 sys.path.insert(0,'./PyGLer/src/pygler/gui')
 
-from utils import ComputeNormals
-from pygler.utils import CreateAxisModel, CreateCubeModel
+#from utils import ComputeNormals
+import pygler
+from pygler.viewer import *
+from pygler.utils import CreateAxisModel, CreateCubeModel, ComputeNormals
 
+import os
 import cv2
 import glob
+import argparse
 
 VertexShaderCode = \
 """
@@ -48,35 +52,45 @@ out vec4 fragPos;
 void main() {
     fragColor = vcolor;
     fragPos = vposition;
+
 }
 """
 
 if __name__ == '__main__':
-    if ((len(sys.argv) < 4 or < len(sys.argv > 4)):
-        print("python main.py scenes_list scannet_dir output_dir")
-    scenes = []
-    with open('{}'.format(sys.argv[1])) as f:
-        scenes = [line.rstrip() for line in f.readlines()]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', required=True)
+    args = parser.parse_args()
+
+    # look up all scene directories
+    scene_dirs = glob.glob(os.path.join(args.data_dir, '*/'))
+    print('Found {} scenes'.format(len(scene_dirs)))
 
     viewer = PyGLer(useFBO=True)
     viewer.start()
-    for scenename in scenes:
-        print('Running ',scenename)
-        tri = PyGLerModel.LoadPly("{0}/scans/{1}/{1}_vh_clean_2.ply".format(sys.argv[2],scenename),autoScale=False)
+    for scenedir in scene_dirs:
+        scenename = os.path.basename(os.path.normpath(scenedir))
 
+        print('Rendering ', scenename)
+        tri = PyGLerModel.LoadPly(os.path.join(scene_dir, '{}_vh_clean_2.ply'.format(scene_dir, scenename)), autoScale=False)
         viewer.addModel(tri)
         
-        n = len(glob.glob('{}/exported/{}/*.color.jpg'.format(sys.argv[2],scenename)))
+        # check trajectory length
+        n = len(glob.glob(os.path.join(scenedir, '*.color.jpg')))
+        print('Trajectory length is ', n)
+
         for i in range(0,n):
-            pose = np.loadtxt('{}/exported/{}/frame-{:06d}.pose.txt'.format(sys.argv[2],scenename,i))
-            
+            pose = np.loadtxt(os.path.join(scenedir, 'frame-{:06d}.pose.txt'.format(i)))
             tri.setModelM(np.linalg.inv(pose))
             
-            depth,bgr = viewer.Convert2BGRD(viewer.capture())
-            bgr = cv2.resize(bgr,(256,192),interpolation=cv2.INTER_NEAREST)
-            depth = cv2.resize(depth,(256,192),interpolation=cv2.INTER_NEAREST)
-            np.save('{}/{}/rendered_normal_{:06d}.npy'.format(sys.argv[3],scenename,i),bgr.astype(np.float32))
-            np.save('{}/{}/rendered_depth_{:06d}.npy'.format(sys.argv[3],scenename,i),depth)
+            depth, bgr = viewer.Convert2BGRD(viewer.capture())
+
+            # do we want resizing here? then do rgb too
+            #bgr = cv2.resize(bgr,(256,192),interpolation=cv2.INTER_NEAREST)
+            #depth = cv2.resize(depth,(256,192),interpolation=cv2.INTER_NEAREST)
+
+            np.save(os.path.join(scene_dir, 'rendered_normal_{:06d}.npy'.format(i)), bgr.astype(np.float32))
+            np.save(os.path.join(scene_dir, 'rendered_depth_{:06d}.npy'.format(i)), depth)
+
             viewer.redraw()
             
 viewer.removeModel(tri)
