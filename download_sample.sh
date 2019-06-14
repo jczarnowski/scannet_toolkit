70afd6896332123d3e6b55b1bbf2bd8a9d93fbc8
Jan Czarnowski <jancz@tuta.io>
2019-06-14 16:25:14 +0100

    Better error handling

--- download_sample.sh
+++ download_sample.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+: ${python:=python} #python will take value "python" if not overriden
+
 reader_dir=ScanNet/SensReader/c++
 reader_exec="$reader_dir/sens"
+
+# make a temporary file for logging
+logfile=$(mktemp /tmp/download-sample-log.XXXXXXX)
+
+die() { echo -e "\n$*" 1>&2 ; echo "See log for more details: $logfile" 1>&2 ; exit 1; }
 
 # Progress bar printing snagged from
 # https://github.com/fearside/ProgressBar
@@ -20,21 +27,21 @@ function download_scene_images {
   _scene_id=$1
   _out_dir=$2
 
-  python download-scannet.py -o . --id ${_scene_id} --type .sens &> /dev/null
+  yes | $python download-scannet.py -o . --id ${_scene_id} --type .sens &>> $logfile || return
 
   # decompress .sens file
   sens_file="scans/${_scene_id}/${_scene_id}.sens"
-  ${reader_exec} ${sens_file} "${_out_dir}/${_scene_id}" &> /dev/null
+  ${reader_exec} ${sens_file} "${_out_dir}/${_scene_id}" &>> $logfile || return
 }
 
 # Downloads model ply file for a given scene 'scene0425_00'
 function download_scene_ply {
   _scene_id=$1
   _out_dir=$2
-  python download-scannet.py -o .  --id ${_scene_id} --type _vh_clean_2.ply &> /dev/null
+  yes | $python download-scannet.py -o .  --id ${_scene_id} --type _vh_clean_2.ply &>> $logfile || return
 
   # move the downloaded ply into proper scene directory
-  mv "scans/${_scene_id}/${_scene_id}_vh_clean_2.ply" "${_out_dir}/${_scene_id}/"
+  mv "scans/${_scene_id}/${_scene_id}_vh_clean_2.ply" "${_out_dir}/${_scene_id}/" || return
 }
 
 # Ensure the reader is compiled
@@ -44,6 +51,13 @@ function compile_reader {
     make -C $reader_dir
   fi
 }
+
+if [[ ! -f "download-scannet.py" ]]; then
+  echo "Please download download-scannet.py by following the instructions on the" \
+  "dataset website (http://www.scan-net.org/ScanNet) and" \
+  "place it alongside this script."
+  exit 1
+fi
 
 # check command line args
 if [ "$#" -ne 2 ]; then
@@ -57,26 +71,27 @@ out_dir=$2
 
 # validate args
 if [[ ! -f $sample_file ]]; then
-  echo "Sample file ${sample_file} does not exist"
-  exit -1
+  die "Sample file ${sample_file} does not exist"
 fi
 
+echo "Logging to: $logfile"
+
 # ensure that sens reader is compiled
-compile_reader || exit
+compile_reader || die "Failed to compile the sens reader"
 
 # probe the input file for number of scenes
 num_scenes=$(cat $1 | wc -l)
 
 # create the output folder
-mkdir -p ${out_dir} || exit
+mkdir -p ${out_dir} || die "Failed to create the output directory"
 
 # download the data
 echo "Downloading data for $num_scenes scenes"
 i=0
 while read scene; do
   progress_bar ${i} ${num_scenes}
-  download_scene_images ${scene} ${out_dir} || exit
-  download_scene_ply ${scene} ${out_dir} || exit
+  download_scene_images ${scene} ${out_dir} || die "Failed to download images for scene ${scene}"
+  download_scene_ply ${scene} ${out_dir} || die "Failed to download model for scene ${scene}"
 
   # clear the downloaded files
   rm -rf scans
